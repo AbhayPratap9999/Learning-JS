6197743d94e2fc44751fb83d4dd191c35df003cd
AbhayPratap9999 <abhaypratapsins99@gmail.com>
2026-02-04 23:57:19 +0530

    Maths in js

--- JS/06_nums_maths.js
+++ JS/06_nums_maths.js
@@ -11,3 +11,17 @@ const otherNumber = 1123.9344;
 console.log(otherNumber.toPrecision(3));
 
 console.log(typeof(score))
+
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 1123.9344;
+console.log(otherNumber.toPrecision(3));
+
+console.log(typeof(score))
