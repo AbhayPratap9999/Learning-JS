6b671b44023f4f92b13af6050197709138102422
AbhayPratap9999 <abhaypratapsins99@gmail.com>
2026-02-05 18:10:37 +0530

    Objects Basics!

--- JS/objects.js
+++ JS/objects.js
@@ -12,12 +12,25 @@ const userDetails ={
 
 }
 
-console.log(userDetails.email)
-console.log(userDetails["email"])
-console.log(userDetails["fullName"])
-console.log(typeof userDetails[mySym])
+// console.log(userDetails.email)
+// console.log(userDetails["email"])
+// console.log(userDetails["fullName"])
+// console.log(typeof userDetails[mySym])
 
 userDetails.email="abhay@github.com"
-Object.freeze(userDetails)
+// Object.freeze(userDetails)
 userDetails.email="abhay@microsoft.com"
-console.log(userDetails["email"])
+//console.log(userDetails["email"])
+
+// add methods
+userDetails.greeting = function () {
+    console.log("Hello JS user");
+};
+
+userDetails.greetingTwo = function () {
+    console.log(`Hello JS user, ${this.fullName}`);
+};
+
+// call methods
+userDetails.greeting();
+userDetails.greetingTwo();
